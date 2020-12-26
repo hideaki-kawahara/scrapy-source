46fb0abbc29408403403fbbc6820c5d3135a2a1f
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2020-12-26 12:06:12 +0900

    update start_urls & add get next page

--- yahoo_news_scrapy/yahoo_news_scrapy/spiders/yahoo_news.py
+++ yahoo_news_scrapy/yahoo_news_scrapy/spiders/yahoo_news.py
@@ -4,7 +4,7 @@ from yahoo_news_scrapy.items import YahooNewsScrapyItem
 class YahooNewsSpider(scrapy.Spider):
     name = 'yahoo_news'
     allowed_domains = ['news.yahoo.co.jp']
-    start_urls = ['https://news.yahoo.co.jp/topics/it']
+    start_urls = ['https://news.yahoo.co.jp/topics/business']
 
     def parse(self, response):
         for items in response.css('li.newsFeed_item'):
@@ -12,3 +12,8 @@ class YahooNewsSpider(scrapy.Spider):
                 url=items.css('a.newsFeed_item_link::attr(href)').extract_first(),
                 title=items.css('div.newsFeed_item_title::text').extract_first()
             )
+
+        next_link = response.css('li.pagination_item-next a::attr(href)').extract_first()
+        if next_link is None:
+            return
+        yield scrapy.Request(response.urljoin(next_link), callback=self.parse)
